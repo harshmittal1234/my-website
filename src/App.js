039674d40de8f080a3dc4c3337e4286b8e1dbdc7
harshmittal1234 <mittalharsh902@gmail.com>
2025-05-24 11:44:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello, welcome to my React website!</h1>
+      <p>This is a simple site built with React.</p>
+    </div>
+  );
+}
+
+export default App;
+
